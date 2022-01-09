3267271ddccec61e803f111a4b2b84e9a28f8a41
Vanesa Hercules <35904949+vnhercules@users.noreply.github.com>
2022-01-09 13:20:33 -0500

    Add progress bar

--- python/extract_unsub_links.py
+++ python/extract_unsub_links.py
@@ -39,7 +39,32 @@ def write_link_to_cell(cell, link):
     """ Write links to Google Sheet"""
     sh.sheet1.update(cell, link)
 
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+
 # Main
+l = len(df)
 for index, row in df.iterrows():
     result = search_for_unsublink(row['body'])
     write_link_to_cell(row['cell'], result)
+    # Update Progress Bar
+    printProgressBar(index + 1, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
